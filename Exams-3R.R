fd3beb563f5443bf94082329e0a5790f2804af24
chukwunonso chukwudozie <anonsoonye2001@yahoo.co.uk>
2019-11-18 16:38:54 -0600

    o

--- Exams-3R.R
+++ Exams-3R.R
@@ -20,4 +20,16 @@ a
 d <- arrange(d, transect.id, dateTime)
 d
 
+#3. Create a directory to store figures (2 points)
+suppressWarnings(dir.create("plots"))
+
+#4. For each transect with a tow type, plot the vertical path the instrument moved through the water
+#column using functions from the ‘ggplot2' library. (10 points)
+#a. Use 'dateTime' as your x-axis values. Set the x-axis tick interval to be 15 minutes with the label ‘hour:minute’.
+
+
+#b. Make sure the water surface is at the top of the plot and the deepest depth is at the bottom of the plot
+#c. Make the points hollow and outlined in dark blue.
+#d. Label each plot with the transect’s name as the title.
+#e. Add a smoother to the plot.
 
